d4ec6b39616204f66bbf63037e198b9c71d55520
Nikita Serhiyovych Krenyov <134860909+nikilodiym@users.noreply.github.com>
2026-03-31 19:08:48 +0300

    commit

--- src/screens/Home/component/Types.jsx
+++ src/screens/Home/component/Types.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import "./Types.css";
 
-import sand from "../../../assets/img/all/sand.png";
-import gravel from "../../../assets/img/all/gravel.png";
-import stone from "../../../assets/img/all/stone.png";
-import dustbin from "../../../assets/img/all/dustbin.png";
-
 export default function Types() {
-    return (
-        <section className="types">
-            <h2>Типи вантажів, які ми перевозимо</h2>
+  return (
+    <section className="types">
+      <h2>Типи вантажів, які ми перевозимо</h2>
 
-            <div className="types__grid">
-                <div className="type-card">
-                    <img src={dustbin} alt="Dustbin" />
-                    <h3>Будівельне сміття</h3>
-                </div>
+      <div className="types__grid">
+        <div className="type-card dustbin">
+          <h3>Будівельне сміття</h3>
+        </div>
 
-                <div className="type-card">
-                    <img src={sand} alt="Sand" />
-                    <h3>Пісок (митий)</h3>
-                </div>
+        <div className="type-card sand">
+          <h3>Пісок (митий)</h3>
+        </div>
 
-                <div className="type-card">
-                    <img src={gravel} alt="Gravel" />
-                    <h3>Щебінь</h3>
-                </div>
+        <div className="type-card gravel">
+          <h3>Щебінь</h3>
+        </div>
 
-                <div className="type-card">
-                    <img src={stone} alt="Stone" />
-                    <h3>Каміння</h3>
-                </div>
-            </div>
-        </section>
-    );
+        <div className="type-card stone">
+          <h3>Каміння</h3>
+        </div>
+      </div>
+    </section>
+  );
 }
